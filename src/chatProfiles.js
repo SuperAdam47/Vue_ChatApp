f4ec785ce537c5aedbc03ec9b9ad31a4d4f03315
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2024-03-19 23:45:29 +0100

    initial commit

---
+++ src/chatProfiles.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 'mattmezza',
+    name: 'Rafal',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
+  },
+  {
+    id: 'support',
+    name: 'Support',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
+  },
+  {
+    id: 'mattmezza',
+    name: 'Rafal',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
+  },
+  {
+    id: 'support',
+    name: 'Support',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
+  }
+]
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Chat from '../../dist/vue-beautiful-chat.umd.min.js'
+import Chat from 'vue-beautiful-chat'
+import vmodal from 'vue-js-modal'
+
+Vue.use(vmodal, {dialog: true})
+Vue.use(Chat)
+
+// eslint-disable-next-line
+new Vue({
+  el: '#app',
+  render: (h) => h(App)
+})
